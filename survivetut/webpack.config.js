f4e2148c4a49c963c2967dbb687f134f2c100c7b
Izaac Zavaleta <jorge.izaac@gmail.com>
2016-02-03 20:24:49 -0700

    modifying gitignore to not ignore build folders

--- survivetut/webpack.config.js
+++ survivetut/webpack.config.js
@@ -33,30 +33,7 @@ const common = {
 
 // Default configuration
 if(TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {
-  devtool: 'eval-source-map',
-  devServer: {
-    contentBase: PATHS.build,
-
-    // Enable history API fallback so HTML5 History API based
-    // routing works. This is a good default that will come
-    // in handy in more complicated setups.
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-
-    // Display only errors to reduce the amount of output.
-    stats: 'errors-only',
-
-    // Parse host and port from env so this is easy to customize.
-    host: process.env.HOST,
-    port: process.env.PORT
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
-  });
+  module.exports = merge(common, {});
 }
 
 if(TARGET === 'build') {
