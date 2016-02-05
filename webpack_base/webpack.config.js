15e553b5319bd114901e51149fdeaa2207292045
Izaac Zavaleta <jorge.izaac@gmail.com>
2016-02-04 19:13:23 -0700

    This is a good basepoint for starting future webpack based projects, copying it in its own sub-directory

---
+++ webpack_base/webpack.config.js
@@ -0,0 +1,41 @@
+/*jshint esversion: 6 */
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  // Entry accepts a path or an object of entries.
+  // The build chapter contains an example of the latter.
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
